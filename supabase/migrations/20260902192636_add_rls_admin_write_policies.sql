980b44856bab393158cd5dcf44473bc365b004b3
Rob Cafferkey <rob_cafferkey@hotmail.co.uk>
2026-09-07 20:40:14 +0100

    Remove duplicate organisation member policies

--- supabase/migrations/20260902192636_add_rls_admin_write_policies.sql
+++ supabase/migrations/20260902192636_add_rls_admin_write_policies.sql
@@ -54,6 +54,9 @@ with check (
 
 -- Organisation Admins can view all memberships
 -- belonging to organisations they administer.
+drop policy if exists "Organisation admins can view organisation members"
+on public.plannix_organisation_users;
+
 create policy "Organisation admins can view organisation members"
 on public.plannix_organisation_users
 for select
@@ -1195,35 +1198,6 @@ with check (
 create policy "Organisation admins can remove year group position users"
 on public.plannix_year_group_position_users
 for delete
-to authenticated
-using (
-  (select private.plannix_is_organisation_admin(organisation_id))
-);
-
--- Organisation Admins can view all memberships
--- belonging to organisations they administer.
-drop policy if exists "Organisation admins can view organisation members"
-on public.plannix_organisation_users;
-
-create policy "Organisation admins can view organisation members"
-on public.plannix_organisation_users
-for select
-to authenticated
-using (
-  (select private.plannix_is_organisation_admin(organisation_id))
-);
-
--- ============================================================
--- ADMIN READ ACCESS TO ORGANISATION MEMBERS
--- ============================================================
-
--- Normal users can already see their own membership row.
--- This additional policy allows Organisation Admins to see
--- all membership rows within organisations they administer.
-
-create policy "Organisation admins can view organisation members"
-on public.plannix_organisation_users
-for select
 to authenticated
 using (
   (select private.plannix_is_organisation_admin(organisation_id))
